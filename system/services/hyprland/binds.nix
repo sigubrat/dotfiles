3b4e01682a790ad9ffb90c9e36ee87c8501ab388
sigubrat <sigurdjbratt@hotmail.no>
2026-03-09 11:01:33 +0100

    fix: layout toggle split keybind after hyprland update

--- system/services/hyprland/binds.nix
+++ system/services/hyprland/binds.nix
@@ -39,7 +39,7 @@ in
       "${mainMod}, Q, killactive"
       "${mainMod}, F, togglefloating"
       "${mainMod}, G, fullscreen"
-      "${mainMod}, P, togglesplit"
+      "${mainMod}, P, layoutmsg, togglesplit"
 
       # Move focus with mainMod + arrow keys
       "${mainMod}, k, movefocus, u"
