06db1270e826a4a12c2f21f3ab2017a6b6854b92
merrinx <gako.footwork856@passinbox.com>
2026-08-12 08:39:09 +0200

    refactor(hyprland): migrate config to Lua, flatten service structure
    - Replace legacy Hyprland NixOS module structure with a single
      system/services/hyprland.nix
    - Remove old split files: binds.nix, rules.nix, settings.nix, tty.nix
    - Move Hyprland configuration to Lua-based config in home-manager module
    - Update package list and session variables for Hyprland
    - Adjust imports in system/services/default.nix to use new structure

---
+++ system/services/hyprland.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
+{
+  imports = [ inputs.hyprland.nixosModules.default ];
+
+  config = lib.mkIf (config.environment.desktop.windowManager == "hyprland") {
+    environment = {
+      systemPackages = [
+        inputs.hyprland-contrib.packages.${pkgs.stdenv.hostPlatform.system}.grimblast
+      ];
+      pathsToLink = [ "/share/icons" ];
+      variables.NIXOS_OZONE_WL = "1";
+    };
+
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      portalPackage =
+        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      plugins = with inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}; [
+        # hyprbars
+        # hyprexpo
+      ];
+    };
+
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      config = {
+        common.default = [ "gtk" ];
+        hyprland.default = [
+          "gtk"
+          "hyprland"
+        ];
+      };
+      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    };
+  };
+}
