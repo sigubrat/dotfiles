6466386e586e9ba31681a60857cd40e15e5637d0
sigubrat <sigurdjbratt@hotmail.no>
2026-08-12 10:27:29 +0200

    refactor(hyprland): add monitor setup command on hyprland start event

--- modules/programs/hyprland.nix
+++ modules/programs/hyprland.nix
@@ -129,6 +129,7 @@ in
       -------------------
 
       hl.on("hyprland.start", function()
+          hl.exec_cmd("setup-monitors")
           hl.exec_cmd("hyprpaper")
           hl.exec_cmd("hyprctl setcursor capitaine-cursors-white 16")
           hl.exec_cmd("wl-clip-persist --clipboard both &")
